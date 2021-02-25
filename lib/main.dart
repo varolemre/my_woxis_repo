bcda9d94af81363f390e282827568d1f3030a411
FlutLab.io <>
2021-02-25 19:39:29 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/woxisapp/generatedkaytolwidget/GeneratedKaytOlWidget.dart';
+import 'package:flutterapp/woxisapp/generatedgiriyapwidget1/GeneratedGiriYapWidget1.dart';
+import 'package:flutterapp/woxisapp/generatedifremiunuttumwidget2/GeneratedifremiunuttumWidget2.dart';
+import 'package:flutterapp/woxisapp/generatedifreyenilemewidget/GeneratedifreYenilemeWidget.dart';
+import 'package:flutterapp/woxisapp/generatedkiiselbilgilerwidget/GeneratedKiiselBilgilerWidget.dart';
+import 'package:flutterapp/woxisapp/generatedantrenmandetaylarwidget/GeneratedAntrenmanDetaylarWidget.dart';
+import 'package:flutterapp/woxisapp/generatedantrenmandetaylar2widget/GeneratedAntrenmanDetaylar2Widget.dart';
+import 'package:flutterapp/woxisapp/generatedantrenmandetaylar3widget/GeneratedAntrenmanDetaylar3Widget.dart';
+import 'package:flutterapp/woxisapp/generatedkiiselbilgileralternatifwidget/GeneratedKiiselBilgileralternatifWidget.dart';
+import 'package:flutterapp/woxisapp/generatedhedefbelirlewidget/GeneratedHedefBelirleWidget.dart';
+import 'package:flutterapp/woxisapp/generatedantrenmankaydetme1widget/GeneratedAntrenmanKaydetme1Widget.dart';
+import 'package:flutterapp/woxisapp/generatedantrenmankaydetme2widget/GeneratedAntrenmanKaydetme2Widget.dart';
+import 'package:flutterapp/woxisapp/generatedstasyonkiiselletirmewidget/GeneratedstasyonKiiselletirmeWidget.dart';
+import 'package:flutterapp/woxisapp/generatedanaekranwidget/GeneratedAnaEkranWidget.dart';
+import 'package:flutterapp/woxisapp/generatedkaytlantrenmanlarwidget1/GeneratedKaytlAntrenmanlarWidget1.dart';
+import 'package:flutterapp/woxisapp/generatedantrenmantipi2widget/GeneratedAntrenmanTipi2Widget.dart';
+import 'package:flutterapp/woxisapp/generatedantrenmanpozisyonuserbestwidget/GeneratedAntrenmanPozisyonuSerbestWidget.dart';
+import 'package:flutterapp/woxisapp/generatedantrenmanpozisyonueksantrikwidget/GeneratedAntrenmanPozisyonuEksantrikWidget.dart';
+import 'package:flutterapp/woxisapp/generatedantrenmanpozisyonusabithzlwidget/GeneratedAntrenmanPozisyonuSabitHzlWidget.dart';
+import 'package:flutterapp/woxisapp/generatedqrkodkiiselletirmewidget/GeneratedQRKodKiiselletirmeWidget.dart';
+import 'package:flutterapp/woxisapp/generatedqrkodantrenmanwidget/GeneratedQRKodAntrenmanWidget.dart';
+import 'package:flutterapp/woxisapp/generatedpkutusubutonueklenecekwidget/GeneratedpkutusubutonueklenecekWidget.dart';
+
+void main() {
+  runApp(WoxisApp());
+}
+
+class WoxisApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedKaytOlWidget',
+      routes: {
+        '/GeneratedKaytOlWidget': (context) => GeneratedKaytOlWidget(),
+        '/GeneratedGiriYapWidget1': (context) => GeneratedGiriYapWidget1(),
+        '/GeneratedifremiunuttumWidget2': (context) =>
+            GeneratedifremiunuttumWidget2(),
+        '/GeneratedifreYenilemeWidget': (context) =>
+            GeneratedifreYenilemeWidget(),
+        '/GeneratedKiiselBilgilerWidget': (context) =>
+            GeneratedKiiselBilgilerWidget(),
+        '/GeneratedAntrenmanDetaylarWidget': (context) =>
+            GeneratedAntrenmanDetaylarWidget(),
+        '/GeneratedAntrenmanDetaylar2Widget': (context) =>
+            GeneratedAntrenmanDetaylar2Widget(),
+        '/GeneratedAntrenmanDetaylar3Widget': (context) =>
+            GeneratedAntrenmanDetaylar3Widget(),
+        '/GeneratedKiiselBilgileralternatifWidget': (context) =>
+            GeneratedKiiselBilgileralternatifWidget(),
+        '/GeneratedHedefBelirleWidget': (context) =>
+            GeneratedHedefBelirleWidget(),
+        '/GeneratedAntrenmanKaydetme1Widget': (context) =>
+            GeneratedAntrenmanKaydetme1Widget(),
+        '/GeneratedAntrenmanKaydetme2Widget': (context) =>
+            GeneratedAntrenmanKaydetme2Widget(),
+        '/GeneratedstasyonKiiselletirmeWidget': (context) =>
+            GeneratedstasyonKiiselletirmeWidget(),
+        '/GeneratedAnaEkranWidget': (context) => GeneratedAnaEkranWidget(),
+        '/GeneratedKaytlAntrenmanlarWidget1': (context) =>
+            GeneratedKaytlAntrenmanlarWidget1(),
+        '/GeneratedAntrenmanTipi2Widget': (context) =>
+            GeneratedAntrenmanTipi2Widget(),
+        '/GeneratedAntrenmanPozisyonuSerbestWidget': (context) =>
+            GeneratedAntrenmanPozisyonuSerbestWidget(),
+        '/GeneratedAntrenmanPozisyonuEksantrikWidget': (context) =>
+            GeneratedAntrenmanPozisyonuEksantrikWidget(),
+        '/GeneratedAntrenmanPozisyonuSabitHzlWidget': (context) =>
+            GeneratedAntrenmanPozisyonuSabitHzlWidget(),
+        '/GeneratedQRKodKiiselletirmeWidget': (context) =>
+            GeneratedQRKodKiiselletirmeWidget(),
+        '/GeneratedQRKodAntrenmanWidget': (context) =>
+            GeneratedQRKodAntrenmanWidget(),
+        '/GeneratedpkutusubutonueklenecekWidget': (context) =>
+            GeneratedpkutusubutonueklenecekWidget(),
+      },
+    );
+  }
+}
